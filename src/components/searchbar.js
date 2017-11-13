1f3511433c8588f363db6667165acc3b17b7693c
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-11-12 17:30:16 -0800

    searchBar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const SearchBar = () =>{
-    return <input />;
+// const SearchBar = () =>{
+//     return <input />;
+// }
+class SearchBar extends React.Component{
+    render(){
+
+    }
 }
 
 export default SearchBar;
