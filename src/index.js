ab6ce1ab648c5d5f74cd11fa34f7ab33a1c54766
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-11-13 17:30:45 -0800

    add VideoDetail

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list'
 import SearchBar from './components/searchbar';
-
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyAm7AqeI5ARrqy4cdqDOUeAVS0fTzLVVBo';
 
 class App extends Component{
 
         constructor(props){
         super(props);
+
         this.state = { videos: []};
         YTSearch({ key: API_KEY, term: 'country music'}, (videos) =>{
             this.setState({ videos: videos});
@@ -21,6 +22,7 @@ class App extends Component{
     return(
         <div>
             <SearchBar />
+            <VideoDetail video = {this.state.videos[0]} />
             <VideoList videos = {this.state.videos} />
         </div>
     );
