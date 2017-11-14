9fa3933ad6b4a3386b61d72683a3fddc77608efe
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-11-14 10:14:26 -0800

    Updates

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Use react to build a VideoSearch;
+Use YouTube API;
+Use webpack to compile;
+URL:https://fscsh.github.io/React_vidio/
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React,{Component} from 'react';
+import _ from 'lodash';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
+import SearchBar from './components/searchbar';
+import VideoDetail from './components/video_detail'
+const API_KEY = 'AIzaSyAm7AqeI5ARrqy4cdqDOUeAVS0fTzLVVBo';
+
+class App extends Component{
+
+        constructor(props){
+        super(props);
+
+        this.state = {
+             videos: [],
+             selectedVideo:null
+         };
+
+         this.videoSearch('country music');
+    }
+
+    videoSearch(term){
+        YTSearch({ key: API_KEY, term: term}, (videos) =>{
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+
+    }
+
+    render(){
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)} , 300);
+
+    return(
+        <div className = 'search-bar'>
+            <SearchBar onSearchTermChange = {videoSearch} />
+            <VideoDetail video = {this.state.selectedVideo} />
+            <VideoList
+            onVideoSelect = {selectedVideo =>this.setState({selectedVideo})}
+            videos = {this.state.videos} />
+        </div>
+    );
+}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
